bb2f42396d9bcbd2000a43909085561bf9de3a89
Aristo Benedict Iskandar <iskandararisto@gmail.com>
2025-10-08 20:06:23 +0800

    Checkpoin4 : Test While Running

--- deep_link_demo/lib/main.dart
+++ deep_link_demo/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uni_links/uni_links.dart';
+import 'package:app_links/app_links.dart';
 import 'dart:async';
 
 void main() => runApp(MyApp());
@@ -10,38 +10,50 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  StreamSubscription? _sub;
+  late final AppLinks _appLinks;
+  StreamSubscription<Uri>? _sub;
   String _status = 'Waiting for link...';
 
   @override
   void initState() {
     super.initState();
-    initUniLinks();
+    initAppLinks();
   }
 
-  Future<void> initUniLinks() async {
-    // 1. Handle initial link if app was launched by a deep link
-    final initialUri = await getInitialUri();
-    if (initialUri != null) _handleIncomingLink(initialUri);
+  Future<void> initAppLinks() async {
+    _appLinks = AppLinks();
 
-    // 2. Handle links while app is running
-    _sub = uriLinkStream.listen((Uri? uri) {
-      if (uri != null) _handleIncomingLink(uri);
-    }, onError: (err) {
-      setState(() => _status = 'Failed to receive link: $err');
-    });
+    try {
+      // 1️⃣ Handle initial link (when app opened from a link)
+      final initialUri = await _appLinks.getInitialLink();
+      if (initialUri != null) {
+        _handleIncomingLink(initialUri);
+      }
+
+      // 2️⃣ Listen for links while app is running
+      _sub = _appLinks.uriLinkStream.listen(
+        (Uri uri) {
+          _handleIncomingLink(uri);
+        },
+        onError: (err) {
+          setState(() => _status = 'Failed to receive link: $err');
+        },
+      );
+    } catch (e) {
+      setState(() => _status = 'Error initializing app links: $e');
+    }
   }
 
   void _handleIncomingLink(Uri uri) {
-    setState(() => _status = 'Received link: $uri');
-
-    if (uri.host == 'details') {
-      // Example link: myapp://details/42
-      final id = uri.pathSegments.isNotEmpty ? uri.pathSegments[0] : 'unknown';
+    // Misal link: myapp://details?id=123
+    if (uri.pathSegments.isNotEmpty && uri.pathSegments.first == 'details') {
+      final id = uri.queryParameters['id'] ?? 'unknown';
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => DetailScreen(id: id)),
+        MaterialPageRoute(builder: (_) => DetailScreen(id: id)),
       );
+    } else {
+      setState(() => _status = 'Opened link: $uri');
     }
   }
 
@@ -56,10 +68,8 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Deep Link Demo',
       home: Scaffold(
-        appBar: AppBar(title: Text('Home')),
-        body: Center(
-          child: Text(_status),
-        ),
+        appBar: AppBar(title: const Text('Home')),
+        body: Center(child: Text(_status)),
       ),
     );
   }
@@ -67,12 +77,12 @@ class _MyAppState extends State<MyApp> {
 
 class DetailScreen extends StatelessWidget {
   final String id;
-  const DetailScreen({required this.id});
+  const DetailScreen({required this.id, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Details')),
+      appBar: AppBar(title: const Text('Details')),
       body: Center(child: Text('You opened item ID: $id')),
     );
   }
